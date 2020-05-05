576c9de99d6bba14f628df57b78b9488caa3fe45
Sarah Tam <sarah.tam17@gmail.com>
2020-05-04 23:08:56 -0700

    Add version 2 of cleaned data, cleaning script

--- clean2.py
+++ clean2.py
@@ -6,7 +6,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize 
 import sys
 
-# Cleans csv files of actual twitter data: remove punctuation, stopwords (as defined by nltk), links
+# Cleans csv files of actual twitter data: remove links
 
 folder = sys.argv[1]
 
@@ -14,27 +14,20 @@ if folder == 'raw_data':
 	keyword = sys.argv[2]
 	week = sys.argv[3]
 	file_in = folder + '/' + keyword + '/' + keyword + '_' + week + '.csv'
-	file_out = folder + '/' + keyword + '_' + week + '_cleaned.txt'
+	file_out = 'cleaned_data_sent' + '/' + keyword + '/' + keyword + '_' + week + '.txt'
 else:
 	month = sys.argv[2]
 	file_in = folder + '/' + month + '.csv'
 	file_out = month + '_cleaned.txt'
 
-str = ''
+lines = []
     
 reader = csv.reader(open(file_in))
 for row in reader:
     # remove links
     toAdd = re.sub(r'\S+.com\S+|http\S+|\S+http\S+', '', row[6])
-    str = str + " " +(toAdd)
-    
-stop_words = set(stopwords.words('english'))
-word_tokens = word_tokenize(str) 
-  
-filtered = [w.lower() for w in word_tokens if not w.lower() in stop_words and w.isalpha()] 
-            
-# return filtered list as string
-cleaned = (" ").join(filtered)
+    lines.append(toAdd)
 
-text_file = open(file_out, "w+")
-text_file.write(cleaned)
+with open(file_out, "w") as text_file:
+	for tweet in lines[1:]:	
+		text_file.write(tweet + '\n')
